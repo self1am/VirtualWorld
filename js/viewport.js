9b760341c79a341fa5c190f80185e9d777715d78
Hanafe Mira <miraahanafee@gmail.com>
2024-05-17 09:40:52 +0530

    touch screen

--- js/viewport.js
+++ js/viewport.js
@@ -1,5 +1,5 @@
-class Viewport{
-    constructor(canvas){
+class Viewport {
+    constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
 
@@ -7,38 +7,45 @@ class Viewport{
         this.center = new Point(canvas.width / 2, canvas.height / 2);
         this.offset = scale(this.center, -1);
         this.drag = {
-            start : new Point(0,0),
-            end : new Point(0,0),
-            offset : new Point(0,0),
-            active : false
+            start: new Point(0, 0),
+            end: new Point(0, 0),
+            offset: new Point(0, 0),
+            active: false
         }
 
         this.#addEventListeners();
     }
 
-    render(){
+    render() {
         this.ctx.restore();
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.save();
         this.ctx.translate(this.center.x, this.center.y);
         this.ctx.scale(1 / this.zoom, 1 / this.zoom);
-        const offset = this.getOffset(); 
+        const offset = this.getOffset();
         this.ctx.translate(offset.x, offset.y);
     }
 
-    getMouse(event, subtractDragOffset = false){
+    getMouse(event, subtractDragOffset = false) {
         const p = new Point(
-            (event.offsetX - this.center.x) * this.zoom - this.offset.x, 
+            (event.offsetX - this.center.x) * this.zoom - this.offset.x,
             (event.offsetY - this.center.y) * this.zoom - this.offset.y
         );
         return subtractDragOffset ? subtract(p, this.drag.offset) : p;
     }
 
-    getOffset(event){
+    getTouchPosition(touch) {
+        return new Point(
+            (touch.clientX - this.center.x) * this.zoom - this.offset.x,
+            (touch.clientY - this.center.y) * this.zoom - this.offset.y
+        );
+    }
+
+    getOffset(event) {
         return add(this.offset, this.drag.offset);
     }
 
-    #addEventListeners(){
+    #addEventListeners() {
         this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
         this.canvas.addEventListener("mousedown", this.#handleMousedown.bind(this));
         this.canvas.addEventListener("mousemove", this.#handleMousemove.bind(this));
@@ -50,33 +57,33 @@ class Viewport{
         this.canvas.addEventListener("touchcancel", this.#handleTouchEnd.bind(this), { passive: false });
     }
 
-    #handleMousedown(event){
-        if(event.button == 1){
+    #handleMousedown(event) {
+        if (event.button == 1) {
             this.drag.start = this.getMouse(event);
             this.drag.active = true;
         }
     }
 
-    #handleMousemove(event){
-        if(this.drag.active){
+    #handleMousemove(event) {
+        if (this.drag.active) {
             this.drag.end = this.getMouse(event);
             this.drag.offset = subtract(this.drag.end, this.drag.start);
         }
     }
 
-    #handleMouseup(event){
-        if(this.drag.active){
+    #handleMouseup(event) {
+        if (this.drag.active) {
             this.offset = add(this.offset, this.drag.offset);
             this.drag = {
-                start : new Point(0,0),
-                end : new Point(0,0),
-                offset : new Point(0,0),
-                active : false
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false
             }
         }
     }
 
-    #handleMouseWheel(event){
+    #handleMouseWheel(event) {
         const dir = Math.sign(event.deltaY);
         const step = 0.1;
         this.zoom += dir * step;
@@ -88,8 +95,8 @@ class Viewport{
         event.preventDefault(); // Prevent default touch behavior
         this.drag.start = this.getTouchPosition(event.touches[0]);
         this.drag.active = true;
-      }
-      
+    }
+
     #handleTouchMove(event) {
         event.preventDefault(); // Prevent default touch behavior
         if (this.drag.active) {
@@ -97,17 +104,17 @@ class Viewport{
             this.drag.offset = subtract(this.drag.end, this.drag.start);
         }
     }
-      
+
     #handleTouchEnd(event) {
         event.preventDefault(); // Prevent default touch behavior
         if (this.drag.active) {
-            this.offset = add(this.offset, this.drag.offset); 
+            this.offset = add(this.offset, this.drag.offset);
             this.drag = {
                 start: new Point(0, 0),
                 end: new Point(0, 0),
                 offset: new Point(0, 0),
                 active: false
-            };    
+            };
         }
     }
 }
